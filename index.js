c9eba8f47c9f607fff89ab6f131b48cd9171281f
Kalpit Shah <72689203+kalpitshah2311@users.noreply.github.com>
2023-08-20 01:01:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Configure MySQL connection
+
+
+
+app.use(express.urlencoded({ extended: false }));
+let student = 
+    [{"std":7,"present":7,"absent":7,"total":14},{"std":8,"present":11,"absent":5,"total":16}]
+;
+app.get('/', (req, res) => {
+    res.json(student);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
